2ef11aac565600b3464a0e99f6d3f1f783a8a48a
Osvaldo Torrez <nachi2083@gmail.com>
2023-12-28 00:09:28 -0300

    Fin seccion6

---
+++ src/app/components/incrementador/incrementador.component.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-incrementador',
+  templateUrl: './incrementador.component.html',
+  styles: [
+  ]
+})
+export class IncrementadorComponent implements OnInit {
+  
+  @Input('valor') progreso: number = 40;
+  @Input() btnClass: string = 'btn btn-primary';
+
+  @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
+
+  ngOnInit() {
+    this.btnClass = `btn ${this.btnClass}`;
+  }
+
+  cambiarValor(valor: number) {
+    if (this.progreso >= 100 && valor >= 0) {
+      this.valorSalida.emit(100);
+      this.progreso = 100;
+    } else if (this.progreso <= 0 && valor <= 0) {
+      this.valorSalida.emit(0);
+      this.progreso = 0;
+    } else {
+      this.progreso = this.progreso + valor;
+      this.valorSalida.emit(this.progreso);
+    }
+  }
+
+  onChange(nuevoValor: number) {
+    if (nuevoValor >= 100) {
+      nuevoValor = 100;
+    } else if (nuevoValor <= 0) {
+      nuevoValor = 0;
+    } else {
+      nuevoValor = nuevoValor
+    }
+    this.valorSalida.emit(nuevoValor);
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { SharesModule } from '../shared/shares.module';
+import { ComponentsModule } from '../components/components.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { SharesModule } from '../shared/shares.module';
 
 
 
@@ -25,8 +28,10 @@ import { SharesModule } from '../shared/shares.module';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     SharesModule,
     RouterModule,
+    ComponentsModule,
   ]
 })
 export class PagesModule { }
